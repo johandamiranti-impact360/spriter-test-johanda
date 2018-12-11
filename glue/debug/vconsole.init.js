349c5145299c72468e71900dccad051875121279
Yew Meng Leong <yew.meng@impact360design.com>
2018-12-11 17:11:14 +0800

    Fix zIndex of vConsole DOM element, so that vConsole is not blocked by cover-start/tap-to-start and re-orient image

--- glue/debug/vconsole.init.js
+++ glue/debug/vconsole.init.js
@@ -7,3 +7,15 @@ Initialization & Configuaration
 
 // init vConsole 
 var vConsole = new VConsole();
+
+// config vConsole
+var zIndex = {
+    "vc-switch": 1000000, 
+    "vc-mask": 1000001, 
+    "vc-panel": 1000002
+};
+
+// change zIndex
+for (vcDom in zIndex) {
+    vConsole.$dom.getElementsByClassName(vcDom)[0].style.zIndex = zIndex[vcDom];
+}
